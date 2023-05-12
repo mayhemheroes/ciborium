5fe154480db29cca9838d4790b2ee1a22df4994b
britton <brittonkfischer@gmail.com>
2023-05-11 23:36:06 -0700

    Remove use

--- fuzz/fuzz_targets/fuzz_from_reader.rs
+++ fuzz/fuzz_targets/fuzz_from_reader.rs
@@ -1,11 +1,9 @@
 use honggfuzz::fuzz;
 
-use ciborium::{de::from_reader, value::Value};
-
 fn main() {
     loop {
         fuzz!(|data: &[u8]| {
-            let _ = from_reader::<Value, _>(&data[..]);
+            let _ = ciborium::de::from_reader::<ciborium::value::Value, _>(&data[..]);
         });
     }
 }
